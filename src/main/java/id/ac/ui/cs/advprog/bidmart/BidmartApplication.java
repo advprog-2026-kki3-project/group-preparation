fbe9238164d68c4a853d33dc8fc017e06023e143
RafaMiyauchi <rafamiyauchi@gmail.com>
2026-02-17 17:48:49 +0700

    First Initialization of the Group Preparation

---
+++ src/main/java/id/ac/ui/cs/advprog/bidmart/BidmartApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.bidmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidmartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BidmartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/bidmart/BidmartApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.bidmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidmartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
