4272451a3aed9e357059e2bb5c7de0b105b24503
ayshialafleur <ayshialafleur@gmail.com>
2026-02-20 18:18:30 +0700

    Set up ci-cd workflows

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
     java
+    jacoco
     id("org.springframework.boot") version "3.5.10"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.sonarqube") version "7.2.2.6593"
 }
 
 group = "id.ac.ui.cs.advprog"
@@ -38,3 +40,33 @@ dependencies {
 tasks.withType<Test> {
     useJUnitPlatform()
 }
+
+tasks.test {
+    filter {
+        includeTestsMatching("*FunctionalTest")
+    }
+
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+
+    reports {
+        xml.required.set(true)
+    }
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "advprog-2026-kki3-project_group-preparation")
+        property("sonar.organization", "advprog-2026-kki3-project")
+        property("sonar.host.url", "https://sonarcloud.io")
+
+        val buildDir = layout.buildDirectory.get().asFile
+        val reportPath = File(buildDir, "reports/jacoco/test/jacocoTestReport.xml").absolutePath
+
+        property("sonar.coverage.jacoco.xmlReportPaths", reportPath)
+    }
+}
+
