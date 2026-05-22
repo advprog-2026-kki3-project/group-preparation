aa3deb43fff2a72cf59885b3e53adb057b7013b5
Bgz1204RI <bagaszharif24@gmail.com>
2026-05-22 15:46:19 +0700

    ____

--- src/main/java/id/ac/ui/cs/advprog/bidmart/BidmartApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/bidmart/BidmartApplication.java
@@ -3,9 +3,7 @@ package id.ac.ui.cs.advprog.bidmart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
-
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
